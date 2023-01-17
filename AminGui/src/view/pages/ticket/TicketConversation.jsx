e5e0cb83390b34057a1c2549a59e587882dc1976
ThongBn <thongbn@outlook.com>
2023-01-17 22:46:29 +0700

    update

--- AminGui/src/view/pages/ticket/TicketConversation.jsx
+++ AminGui/src/view/pages/ticket/TicketConversation.jsx
@@ -1,11 +1,47 @@
-import { Avatar, Image, Comment, Skeleton, Tooltip, List } from "antd";
+import {Avatar, Button, Form, Comment, Skeleton, Tooltip, List, Input, Space, Select, Row} from "antd";
 import moment from "moment";
-import React, { useEffect, useState, memo } from "react";
+import React, {useEffect, useState, memo} from "react";
+import {RiSendPlaneLine} from "react-icons/ri"
 
-const TicketConversation = ({ dataSource, isLoading }) => {
+const {TextArea} = Input;
 
+const TicketConversation = ({dataSource, isLoading}) => {
+
+    const [form] = Form.useForm();
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(isLoading);
+
+    const CommentForm = () => {
+        return (<Form
+            form={form}
+            layout="vertical"
+            name="basic"
+            size={"small"}
+            onFinish={onReplyComment}
+        >
+            <Form.Item>
+                <Select placeholder={"Nhập id, từ khóa của template"}
+                        showSearch
+                        onSearch={onSearchTemplate}
+                        onSelect={onSelectTemplate}/>
+            </Form.Item>
+            <Form.Item name="message">
+                <TextArea rows={3} placeholder={"Nhập phản hồi"}/>
+            </Form.Item>
+            <Form.Item>
+                <Space size="small">
+                    <Button type="link" icon={<i className="ri-attachment-2-line"/>}>Add Attachment</Button>
+                </Space>
+            </Form.Item>
+            <Row justify={"end"}>
+                <Space size='small'>
+                    <Button type="primary" icon={<RiSendPlaneLine className="remix-icon"/>}>
+                        Send
+                    </Button>
+                </Space>
+            </Row>
+        </Form>)
+    };
 
     useEffect(() => {
         setData(dataSource);
@@ -15,10 +51,24 @@ const TicketConversation = ({ dataSource, isLoading }) => {
         setLoading(isLoading);
     }, [isLoading]);
 
+
+    const onReplyComment = () => {
+
+    };
+
+
+    const onSearchTemplate = () => {
+
+    };
+
+    const onSelectTemplate = () => {
+
+    }
+
     const fakeData = [
         {
             author: "Dolores O'Riordan",
-            avatar: <Avatar src="https://picsum.photos/200/300" />,
+            avatar: <Avatar src="https://picsum.photos/200/300"/>,
             content: (
                 <p>
                     We supply a series of design principles, practical patterns and high
@@ -35,8 +85,8 @@ const TicketConversation = ({ dataSource, isLoading }) => {
             ),
         },
         {
-            author: "Dolores O'Riordan",
-            avatar: <Avatar src="https://picsum.photos/200/300" />,
+            author: "Aeon Mall",
+            avatar: <Avatar src="https://picsum.photos/200/300"/>,
             content: (
                 <p>
                     We supply a series of design principles, practical patterns and high
@@ -52,7 +102,13 @@ const TicketConversation = ({ dataSource, isLoading }) => {
                 </Tooltip>
             ),
         },
+        {
+            author: "Aeon Mall",
+            avatar: <Avatar src="https://picsum.photos/200/300"/>,
+            content: <CommentForm/>,
+        }
     ];
+
 
     return (
         <Skeleton loading={loading} active>
@@ -73,6 +129,7 @@ const TicketConversation = ({ dataSource, isLoading }) => {
                     </li>
                 )}
             />
+
         </Skeleton>
     )
 }
--- AminGui/src/view/pages/ticket/detail.jsx
+++ AminGui/src/view/pages/ticket/detail.jsx
@@ -1,14 +1,15 @@
-import React, { memo, useEffect, useState } from "react";
-import { Avatar, Card, Col, Row, Segmented, Skeleton, Space, Tag, Typography } from "antd";
-import { useHistory, useParams } from "react-router-dom";
+import React, {memo, useEffect, useState} from "react";
+import {Button, Card, Col, Row, Segmented, Skeleton, Space, Tag, Typography} from "antd";
+import {useHistory, useParams} from "react-router-dom";
 import TicketConversation from "./TicketConversation";
 import TicketTags from "./TicketTags";
-import { Check } from "iconsax-react";
+import {RiStickyNoteFill, RiInstagramLine} from "react-icons/ri";
 import TicketLogs from "./TicketLogs";
 import TicketOtherInfo from "./TicketOtherInfo";
+import TicketLatestCase from "./TicketLastestCase";
 
 const Page = () => {
-    const { id } = useParams();
+    const {id} = useParams();
     const [loading, setLoading] = useState(false);
     const [ticketStatus, setTicketStatus] = useState("1_new");
     const [activeTab, setActiveTab] = useState("conversation");
@@ -31,12 +32,17 @@ const Page = () => {
             key: 'info',
             tab: 'Information',
         },
+        {
+            key: 'latestCase',
+            tab: 'Latest Case',
+        },
     ];
 
     const contentList = {
-        conversation: <TicketConversation dataSource={null} isLoading={loading} />,
-        log: <TicketLogs id={id} />,
-        info: <TicketOtherInfo />,
+        conversation: <TicketConversation dataSource={null} isLoading={loading}/>,
+        log: <TicketLogs id={id}/>,
+        info: <TicketOtherInfo/>,
+        latestCase: <TicketLatestCase/>
     };
 
     const onTabChange = (key) => {
@@ -45,13 +51,21 @@ const Page = () => {
 
     return (
         <Row gutter={[8, 8]}>
+            <Col xs={24}>
+                <Space>
+                    <Typography.Title level={4} style={{marginBottom: 0}}>
+                        <RiInstagramLine className="remix-icon"/> #{id}
+                    </Typography.Title>
+                    <Tag color="">New</Tag>
+                </Space>
+            </Col>
             <Col md={16}>
-                <Space direction="vertical" size={"small"} style={{ width: "100%" }}>
+                <Space direction="vertical" size={"small"} style={{width: "100%"}}>
                     <Card size="small"
-                        tabList={tabList}
-                        tabProps={{ size: "small" }}
-                        activeTabKey={activeTab}
-                        onTabChange={onTabChange}
+                          tabList={tabList}
+                          tabProps={{size: "small"}}
+                          activeTabKey={activeTab}
+                          onTabChange={onTabChange}
                     >
                         <Skeleton loading={loading} active>
                             {contentList[activeTab]}
@@ -60,31 +74,39 @@ const Page = () => {
                 </Space>
             </Col>
             <Col md={8}>
-                <Space direction="vertical" size={"small"} style={{ width: "100%" }}>
+                <Space direction="vertical" size={"small"} style={{width: "100%"}}>
                     <Card size="small" title={`Trạng thái`}>
                         <Skeleton loading={loading} active>
-                            <Segmented
-                                block
-                                options={[
-                                    {
-                                        label: (
-                                            <Typography.Text type="secondary">New</Typography.Text>
-                                        ),
-                                        value: "1_new",
-                                    },
-                                    {
-                                        label: <Typography.Text type="warning">Processing</Typography.Text>,
-                                        value: "2_processing",
-                                    },
-                                    {
-                                        label: <Typography.Text type="success">Done</Typography.Text>,
-                                        value: "3_done",
-                                    }
-                                ]} />
+                            <Space direction="vertical" style={{width: "100%"}}>
+                                <Segmented
+                                    size="small"
+                                    block
+                                    options={[
+                                        {
+                                            label: (
+                                                <Typography.Text type="secondary">New</Typography.Text>
+                                            ),
+                                            value: "1_new",
+                                        },
+                                        {
+                                            label: <Typography.Text type="warning">Processing</Typography.Text>,
+                                            value: "2_processing",
+                                        },
+                                        {
+                                            label: <Typography.Text type="success">Done</Typography.Text>,
+                                            value: "3_done",
+                                        }
+                                    ]}/>
+                                <Button
+                                    size='small'
+                                    icon={<RiStickyNoteFill className="remix-icon"/>}>
+                                    Add note
+                                </Button>
+                            </Space>
                         </Skeleton>
                     </Card>
                     <Card size="small" title={`Tags`}>
-                        <TicketTags id={id} />
+                        <TicketTags id={id}/>
                     </Card>
                 </Space>
             </Col>
